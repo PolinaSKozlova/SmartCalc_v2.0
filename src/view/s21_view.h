616a771d4dcfb510fd015eebb43f2fe7fbf34ccc
verenach <verenach@student.21-school.ru>
2023-04-07 19:52:07 +0700

    перенсла методы в с

--- src/view/s21_view.h
+++ src/view/s21_view.h
@@ -4,25 +4,27 @@
 #include <stack>
 #include <string>
 
+enum TokenPriority { ZERO, FIRST, SECOND, THIRD, FOURTH };
+
 namespace s21 {
 class View {
  public:
   View() : calc_notation_() {}
   ~View() { ClearStack(); }
-  bool CheckBracets();
-  struct Token {
-    double value;
-    int priority;
-    char type;
-  };
-  std::stack<Token> calc_notation_;
+  bool CheckHooks(std::string src);
+  bool CheckDots(std::string src);
+  bool IsFunction(std::string src);
+  bool IsOperator(std::string src);
+  bool IsHooks(std::string src);
 
  private:
-  void ClearStack() {
-    while (calc_notation_.size()) {
-      calc_notation_.pop();
-    }
-  }
+  void ClearStack();
+  struct Token {
+    double value_;
+    int priority_;
+    char type_;
+  };
+  std::stack<Token> calc_notation_;
 };
 };  // namespace s21
 
