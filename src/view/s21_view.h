337abcb4558a147a7959aee99b661992211a2c37
verenach <verenach@student.21-school.ru>
2023-04-07 19:33:46 +0700

    добавила конструктор и деструктор, структуру для хранения токенов

--- src/view/s21_view.h
+++ src/view/s21_view.h
@@ -1,10 +1,28 @@
 #ifndef S21_VIEW_H_
 #define S21_VIEW_H_
 
+#include <stack>
+#include <string>
+
 namespace s21 {
 class View {
  public:
+  View() : calc_notation_() {}
+  ~View() { ClearStack(); }
+  bool CheckBracets();
+  struct Token {
+    double value;
+    int priority;
+    char type;
+  };
+  std::stack<Token> calc_notation_;
+
  private:
+  void ClearStack() {
+    while (calc_notation_.size()) {
+      calc_notation_.pop();
+    }
+  }
 };
 };  // namespace s21
 
