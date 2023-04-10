91d582f40a546c7c447fd2c53056e3a422d564d6
verenach <verenach@student.21-school.ru>
2023-04-10 15:13:21 +0700

    добавила поля класса и конструктор копирования

--- src/controller/s21_controller.h
+++ src/controller/s21_controller.h
@@ -1,10 +1,23 @@
 #ifndef S21_CONTROLLER_H_
 #define S21_CONTROLLER_H_
 
+#include "../model/s21_model.h"
+#include "../view/s21_view.h"
+
 namespace s21 {
 class Controller {
  public:
+  Controller();
+  Controller(Model *other) : model_(other) {}
+  Controller &operator=(Model *other) {
+    model_ = other;
+    return *this;
+  }
+  ~Controller();
+
  private:
+  Model *model_;
+  View *view_;
 };
 };  // namespace s21
 
