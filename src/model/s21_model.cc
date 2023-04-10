2a2f0458feab18507661aafa9b42dba6877f5910
verenach <verenach@student.21-school.ru>
2023-04-10 16:30:30 +0700

    добавила проверку скобок

--- src/model/s21_model.cc
+++ src/model/s21_model.cc
@@ -5,3 +5,32 @@ void s21::Model::ClearStack() {
     input_.pop();
   }
 }
+
+bool s21::Model::CheckHooks(std::string src) noexcept {
+  int count_hooks = 0;
+  bool result = true;
+  for (size_t i = 0; i < src.length() && count_hooks >= 0; ++i) {
+    if (src[i] == '(') {
+      ++count_hooks;
+    }
+    if (src[i] == ')') {
+      --count_hooks;
+    }
+    if (src[i] == '(' && src[i + 1] == ')') {
+      result = false;
+      break;
+    }
+  }
+  if (count_hooks) {
+    result = false;
+  }
+  return result;
+}
+
+// bool CheckDots(std::string src) {}
+
+// bool IsFunction(std::string src) {}
+
+// bool IsOperator(std::string src) {}
+
+// bool IsHooks(std::string src) {}
--- src/model/s21_model.h
+++ src/model/s21_model.h
@@ -4,29 +4,38 @@
 #include <stack>
 #include <string>
 
-#include "../controller/s21_controller.h"
+// #include "../controller/s21_controller.h"
 
 enum TokenPriority { ZERO, FIRST, SECOND, THIRD, FOURTH };
 
 namespace s21 {
 class Model {
  public:
-  Model();
+  Model(std::string other) : src_(other) {}
   ~Model() { ClearStack(); }
-  bool CheckHooks(std::string src);
-  bool CheckDots(std::string src);
-  bool IsFunction(std::string src);
-  bool IsOperator(std::string src);
-  bool IsHooks(std::string src);
+  bool CheckHooks(std::string src) noexcept;
+  bool CheckDots(std::string src) noexcept;
+  bool IsFunction(std::string src) noexcept;
+  bool IsOperator(std::string src) noexcept;
+  bool IsHooks(std::string src) noexcept;
 
  private:
   void ClearStack();
   struct Token {
+    // нужен ли конструктор и деструктор
+    Token(double value, int priority, char type)
+        : value_(value), priority_(priority), type_(type) {}
+    ~Token() {
+      value_ = 0;
+      priority_ = 0;
+      type_ = '\0';
+    }
     double value_;
     int priority_;
     char type_;
   };
   std::stack<Token> input_;
+  std::string src_;
 };
 };  // namespace s21
 
