697a31482dbc16dd4fe5261d9a90133da8afacad
verenach <verenach@student.21-school.ru>
2023-04-20 16:45:53 +0700

    вынесла класс кредитный калькулятор в отдельный файл

---
+++ src/model/s21_credit_calculator.h
@@ -0,0 +1,16 @@
+#ifndef SRC_S21_CREDIT_CALCULATOR_H_
+#define SRC_S21_CREDIT_CALCULATOR_H_
+
+#include <cmath>
+
+namespace s21 {
+class CreditCalculator {
+ public:
+  CreditCalculator() {}
+  ~CreditCalculator() = default;
+
+ private:
+};
+};  // namespace s21
+
+#endif  // SRC_S21_CREDIT_CALCULATOR_H_
