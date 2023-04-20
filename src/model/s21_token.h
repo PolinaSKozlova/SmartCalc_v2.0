56b0e3b54ba2e474853f78cb1fc4b2eb253fa9a3
verenach <verenach@student.21-school.ru>
2023-04-20 16:52:37 +0700

    вынесла класс Token, map valid_tokens, enum Priority в отдельный файл

---
+++ src/model/s21_token.h
@@ -0,0 +1,53 @@
+#ifndef SRC_S21_TOKEN_H_
+#define SRC_S21_TOKEN_H_
+
+#include <cctype>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+namespace s21 {
+enum class Priority { kZero = 0, kFirst, kSecond, kThird, kFourth };
+
+class Token {
+ public:
+  explicit Token(double value, s21::Priority priority, std::string type,
+                 bool is_binary = false)
+      : value_(value),
+        priority_(priority),
+        type_(type),
+        is_binary_(is_binary) {}
+  ~Token() = default;
+  double value_;
+  s21::Priority priority_;
+  std::string type_;
+  bool is_binary_;
+};
+
+const std::map<std::string, Token> valid_tokens{
+    // const перед string
+    {"x", Token(0.0, s21::Priority::kZero, "x")},
+    {"+", Token(0.0, s21::Priority::kFirst, "sum", true)},
+    {"-", Token(0.0, s21::Priority::kFirst, "sub", true)},
+    {"*", Token(0.0, s21::Priority::kSecond, "*", true)},
+    {"/", Token(0.0, s21::Priority::kSecond, "/", true)},
+    {"(", Token(0.0, s21::Priority::kThird, "(")},
+    {")", Token(0.0, s21::Priority::kThird, ")")},
+    {"^", Token(0.0, s21::Priority::kThird, "^", true)},
+    {"cos", Token(0.0, s21::Priority::kFourth, "cos")},
+    {"sin", Token(0.0, s21::Priority::kFourth, "sin")},
+    {"tan", Token(0.0, s21::Priority::kFourth, "tan")},
+    {"acos", Token(0.0, s21::Priority::kFourth, "acos")},
+    {"asin", Token(0.0, s21::Priority::kFourth, "asin")},
+    {"atan", Token(0.0, s21::Priority::kFourth, "atan")},
+    {"sqrt", Token(0.0, s21::Priority::kFourth, "sqrt")},
+    {"ln", Token(0.0, s21::Priority::kFourth, "ln")},
+    {"log", Token(0.0, s21::Priority::kFourth, "log")},
+    {"mod", Token(0.0, s21::Priority::kFourth, "%", true)}};
+};  // namespace s21
+
+#endif  // SRC_S21_TOKEN_H_
--- src/model/s21_tokenizer.h
+++ src/model/s21_tokenizer.h
@@ -1,54 +1,9 @@
 #ifndef SRC_S21_TOKENIZER_H_
 #define SRC_S21_TOKENIZER_H_
 
-#include <cctype>
-#include <iostream>
-#include <map>
-#include <regex>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <vector>
+#include "s21_token.h"
 
 namespace s21 {
-enum class Priority { kZero = 0, kFirst, kSecond, kThird, kFourth };
-
-class Token {
- public:
-  Token(double value, s21::Priority priority, std::string type,
-        bool is_binary = false)
-      : value_(value),
-        priority_(priority),
-        type_(type),
-        is_binary_(is_binary) {}
-  ~Token() = default;
-  double value_;
-  s21::Priority priority_;
-  std::string type_;
-  bool is_binary_;
-};
-
-const std::map<std::string, Token> valid_tokens{
-    // const перед string
-    {"x", Token(0.0, s21::Priority::kZero, "x")},
-    {"+", Token(0.0, s21::Priority::kFirst, "sum", true)},
-    {"-", Token(0.0, s21::Priority::kFirst, "sub", true)},
-    {"*", Token(0.0, s21::Priority::kSecond, "*", true)},
-    {"/", Token(0.0, s21::Priority::kSecond, "/", true)},
-    {"(", Token(0.0, s21::Priority::kThird, "(")},
-    {")", Token(0.0, s21::Priority::kThird, ")")},
-    {"^", Token(0.0, s21::Priority::kThird, "^", true)},
-    {"cos", Token(0.0, s21::Priority::kFourth, "cos")},
-    {"sin", Token(0.0, s21::Priority::kFourth, "sin")},
-    {"tan", Token(0.0, s21::Priority::kFourth, "tan")},
-    {"acos", Token(0.0, s21::Priority::kFourth, "acos")},
-    {"asin", Token(0.0, s21::Priority::kFourth, "asin")},
-    {"atan", Token(0.0, s21::Priority::kFourth, "atan")},
-    {"sqrt", Token(0.0, s21::Priority::kFourth, "sqrt")},
-    {"ln", Token(0.0, s21::Priority::kFourth, "ln")},
-    {"log", Token(0.0, s21::Priority::kFourth, "log")},
-    {"mod", Token(0.0, s21::Priority::kFourth, "%", true)}};
-
 class Tokenizer {
  public:
   Tokenizer() = default;
