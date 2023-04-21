ce97b0ae369ea007dcb8b47452b64a4bbe4f0645
verenach <verenach@student.21-school.ru>
2023-04-21 17:47:33 +0700

    добавила метод CountResult

--- src/model/s21_model.cc
+++ src/model/s21_model.cc
@@ -9,7 +9,25 @@ void s21::MathCalculator::CalculateResultFromInput() {
   }
 }
 
-void s21::MathCalculator::CountResult() noexcept {}
+void s21::MathCalculator::CountResult() noexcept {
+  std::stack<Token> my_stack;
+  for (auto& current_token : output_tokens_) {
+    if (current_token.priority_ == s21::Priority::kZero) {
+      my_stack.push(current_token);
+    } else if (current_token.is_binary_) {
+      s21::Token arg_1 = my_stack.top();
+      my_stack.pop();
+      s21::Token arg_2 = my_stack.top();
+      my_stack.pop();
+      // my_stack.push();
+    } else {
+      s21::Token arg_1 = my_stack.top();
+      my_stack.pop();
+      // my_stack.push();
+    }
+  }
+  answer_ = my_stack.top().value_;
+}
 
 double s21::MathCalculator::GetAnswer() const noexcept { return answer_; }
 
