93b0bb277fc48e916e98a25bacdb962c94576f15
verenach <verenach@student.21-school.ru>
2023-04-24 16:27:51 +0700

    добавила в кредитный калькулятор класс для передачи информации

--- src/model/s21_credit_calculator.h
+++ src/model/s21_credit_calculator.h
@@ -4,12 +4,36 @@
 #include <cmath>
 
 namespace s21 {
+class CreditInformation {
+ public:
+  CreditInformation(double credit_sum, int credit_term, double interest_rate,
+                    double monthly_payment, double payble_percents,
+                    double total_sum, bool term_in_years)
+      : credit_sum_(credit_sum),
+        credit_term_(credit_term),
+        interest_rate_(interest_rate),
+        monthly_payment_(monthly_payment),
+        payble_percents_(payble_percents),
+        total_sum_(total_sum),
+        term_in_years_(term_in_years) {}
+  ~CreditInformation() = default;
+  CreditInformation GetInformation() const noexcept { return *this; }
+  double credit_sum_;
+  int credit_term_;
+  double interest_rate_;
+  double monthly_payment_;
+  double payble_percents_;
+  double total_sum_;
+  bool term_in_years_;
+};
+
 class CreditCalculator {
  public:
   CreditCalculator() {}
   ~CreditCalculator() = default;
 
  private:
+  CreditInformation data_;
 };
 };  // namespace s21
 
