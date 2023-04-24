3bc3e4be8d9b8cd40167de3792b7dc21cc5cf245
verenach <verenach@student.21-school.ru>
2023-04-24 16:53:20 +0700

    добавила расчет кредита аннуитетным и дифференциированным способами

--- src/model/s21_credit_calculator.cc
+++ src/model/s21_credit_calculator.cc
@@ -1,1 +1,37 @@
 #include "s21_credit_calculator.h"
+
+static double RoundNumber(double value) { return round(value * 100) / 100; }
+
+void s21::CreditCalculator::AnnuitetMethod() {
+  data_.FromYearsToMonths();
+  data_.interest_rate_ = data_.interest_rate_ / 100 / 12;
+  data_.monthly_payment_.front() =
+      data_.credit_sum_ *
+      (data_.interest_rate_ /
+       (1 - pow((1 + data_.interest_rate_), (data_.credit_term_ * -1))));
+  data_.monthly_payment_.front() = RoundNumber(data_.monthly_payment_.front());
+  data_.total_sum_ = data_.monthly_payment_.front() * data_.credit_term_;
+  data_.payble_percents_ = data_.total_sum_ - data_.credit_sum_;
+}
+
+void s21::CreditCalculator::DifferntiatedMethod() {
+  data_.FromYearsToMonths();
+  data_.interest_rate_ = data_.interest_rate_ / 100;
+  double pay = 0;
+  double perc_sum = 0;
+  pay = data_.credit_sum_ / data_.credit_term_;
+  data_.total_sum_ = data_.credit_sum_;
+  for (int i = 0; i < data_.credit_term_; ++i) {
+    data_.payble_percents_ = (data_.credit_sum_ * data_.interest_rate_) / 12;
+    data_.payble_percents_ = RoundNumber(data_.payble_percents_);
+    data_.monthly_payment_[i] = data_.payble_percents_ + pay;
+    perc_sum += data_.payble_percents_;
+    data_.credit_sum_ -= pay;
+  }
+  data_.total_sum_ += perc_sum;
+  data_.payble_percents_ = perc_sum;
+}
+
+int s21::CreditInformation::FromYearsToMonths() {
+  return term_in_years_ ? credit_term_ * 12 : credit_term_;
+}
--- src/model/s21_credit_calculator.h
+++ src/model/s21_credit_calculator.h
@@ -2,12 +2,13 @@
 #define SRC_S21_CREDIT_CALCULATOR_H_
 
 #include <cmath>
+#include <vector>
 
 namespace s21 {
 class CreditInformation {
  public:
   CreditInformation(double credit_sum, int credit_term, double interest_rate,
-                    double monthly_payment, double payble_percents,
+                    std::vector<double> monthly_payment, double payble_percents,
                     double total_sum, bool term_in_years)
       : credit_sum_(credit_sum),
         credit_term_(credit_term),
@@ -18,10 +19,11 @@ class CreditInformation {
         term_in_years_(term_in_years) {}
   ~CreditInformation() = default;
   CreditInformation GetInformation() const noexcept { return *this; }
+  int FromYearsToMonths();
   double credit_sum_;
   int credit_term_;
   double interest_rate_;
-  double monthly_payment_;
+  std::vector<double> monthly_payment_;
   double payble_percents_;
   double total_sum_;
   bool term_in_years_;
@@ -29,8 +31,11 @@ class CreditInformation {
 
 class CreditCalculator {
  public:
-  CreditCalculator() {}
+  CreditCalculator(const CreditInformation& data) : data_(data) {}
   ~CreditCalculator() = default;
+  CreditInformation GetData() { data_.GetInformation(); }
+  void AnnuitetMethod() {}
+  void DifferntiatedMethod() {}
 
  private:
   CreditInformation data_;
