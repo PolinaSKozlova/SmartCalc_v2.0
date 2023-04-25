44905b0017340174ba00e67dc8c53c2c9c3a0d57
verenach <verenach@student.21-school.ru>
2023-04-25 20:58:45 +0700

    увеличила в методе GetStringAnswer precision

--- src/model/s21_model.cc
+++ src/model/s21_model.cc
@@ -1,13 +1,13 @@
 #include "s21_model.h"
 
 void s21::MathCalculator::CalculateResultFromInput() {
-  try {
-    output_tokens_ = tokens_notation_.CreateNotation();
-    CountResult();
-  } catch (std::invalid_argument& e) {
-    // std::cerr << e.what() << std::endl;
-    throw e;
-  }
+  // try {
+  output_tokens_ = tokens_notation_.CreateNotation();
+  CountResult();
+  // } catch (std::invalid_argument& e) {
+  // std::cerr << e.what() << std::endl;
+  //   throw e;
+  // }
 }
 
 void s21::MathCalculator::CountResult() noexcept {
@@ -36,7 +36,7 @@ double s21::MathCalculator::GetAnswer() const noexcept { return answer_; }
 
 std::string s21::MathCalculator::GetStringAnswer() const noexcept {
   std::ostringstream oss;
-  oss.precision(7);
+  oss.precision(10);
   oss << answer_;
   return oss.str();
 }
