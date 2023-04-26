0f6d7102213dfb873b0edce33f60e07bb2372d67
verenach <verenach@student.21-school.ru>
2023-04-26 18:25:27 +0700

    изменила название в гардах, изменила в методе ParceAndCalculateExpression возвращаемый тип

--- src/controller/s21_controller.h
+++ src/controller/s21_controller.h
@@ -1,9 +1,8 @@
-#ifndef S21_CONTROLLER_H_
-#define S21_CONTROLLER_H_
+#ifndef SMARTCALC_S21_CONTROLLER_H_
+#define SMARTCALC_S21_CONTROLLER_H_
 
-// #include "../model/s21_extra_classes.h"
 #include "../model/s21_model.h"
-#include "../view/s21_view.h"
+// #include "../view/s21_view.h"
 
 namespace s21 {
 class Controller {
@@ -12,24 +11,24 @@ class Controller {
   explicit Controller(const std::string &src, const std::string &x = "0.0")
       : model_(src, x) {}
   ~Controller() = default;
-  std::string ParceAndCalculateExpression() noexcept {
-    std::string count_result{};
+  void ParceAndCalculateExpression() noexcept {
     try {
       model_.CalculateResultFromInput();
-      count_result = model_.GetStringAnswer();
+      output_ = model_.GetStringAnswer();
     } catch (std::invalid_argument &e) {
-      count_result = e.what();
+      output_ = e.what();
     }
-    return count_result;
+    // return output_;
   }
   std::string GetAnswer() const { return model_.GetStringAnswer(); }
+  std::string GetOutputAnswer() const { return output_; }
 
  private:
   s21::MathCalculator model_;
-  std::string output_;
-  std::string x_value_;
-  // View *view_;
+  std::string output_{};
+  // std::string x_value_;
+  // s21::View *view_;
 };
 };  // namespace s21
 
-#endif  // S21_CONTROLLER_H_
+#endif  // SMARTCALC_S21_CONTROLLER_H_
