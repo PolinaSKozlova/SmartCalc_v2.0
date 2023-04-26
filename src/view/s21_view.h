9b10e875fac6accfb430da4a8bd3343375978e56
Your Name <you@example.com>
2023-04-26 10:57:32 +0000

    добавила геттер, удалила файл cc

--- src/view/s21_view.h
+++ src/view/s21_view.h
@@ -9,17 +9,18 @@
 namespace s21 {
 class View {
  public:
-  View(const std::string& other) : src_(other) {}
+  View(const std::string& other, const std::string x_value = "0.0")
+      : controller_(other, x_value) {}
   // explicit View(Controller* other) : controller_(other) {}
   // View& operator=(Controller* other) {
   //   controller_ = other;
   //   return *this;
   // }
   ~View() {}
+  std::string GetAnswer() const { return controller_.GetAnswer(); }
 
  private:
-  // Controller* controller_;
-  std::string src_;
+  Controller controller_;
 };
 };  // namespace s21
 
