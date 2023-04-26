761f92506fbf8c04797d481163a566ac444b8e6d
verenach <verenach@student.21-school.ru>
2023-04-26 18:26:20 +0700

    изменила название в гардах, добавила геттер

--- src/view/s21_view.h
+++ src/view/s21_view.h
@@ -1,5 +1,5 @@
-#ifndef S21_VIEW_H_
-#define S21_VIEW_H_
+#ifndef SMARTCALC_S21_VIEW_H_
+#define SMARTCALC_S21_VIEW_H_
 
 #include <stack>
 #include <string>
@@ -9,19 +9,19 @@
 namespace s21 {
 class View {
  public:
-  View(const std::string& other, const std::string x_value = "0.0")
+  explicit View(const std::string& other, const std::string& x_value = "0.0")
       : controller_(other, x_value) {}
   // explicit View(Controller* other) : controller_(other) {}
   // View& operator=(Controller* other) {
-  //   controller_ = other;
+  //   controller_ = other;s
   //   return *this;
   // }
   ~View() {}
   std::string GetAnswer() const { return controller_.GetAnswer(); }
 
  private:
-  Controller controller_;
+  s21::Controller controller_;
 };
 };  // namespace s21
 
-#endif  // S21_VIEW_H_
+#endif  // SMARTCALC_S21_VIEW_H_
