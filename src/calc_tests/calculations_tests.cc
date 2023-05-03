8872c1800c58006090d04b24c1c5751f11a8259b
verenach <verenach@student.21-school.ru>
2023-05-03 15:44:49 +0700

    добавила кейсы

--- src/calc_tests/calculations_tests.cc
+++ src/calc_tests/calculations_tests.cc
@@ -5,258 +5,225 @@
 #define ACCURACY 1e-07
 
 TEST(calculation_src_test, test_1) {
-  std::string src = "sqrt(5647*(9-(-78)))+46^3/250-log(1000)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("sqrt(5647*(9-(-78)))+46^3/250-log(1000)");
   double result = 1087.2641095702704976;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_2) {
-  std::string src = "+986*0.0000001-(91mod3)^3";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("+986*0.0000001-(91mod3)^3");
   double result = -0.9999014;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_3) {
-  std::string src = "10000000+98/100000-cos(90)*(sqrt(625))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000+98/100000-cos(90)*(sqrt(625))");
   double result = 10000011.2028204;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_4) {
-  std::string src = "log(-178)+634/2/0.5";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("log(-178)+634/2/0.5");
   m.CalculateResultFromInput();
   EXPECT_TRUE(std::isnan(m.GetAnswer()));
 }
 
 TEST(calculation_src_test, test_5) {
-  std::string src = "log(178)+634/2/0.5^3";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("log(178)+634/2/0.5^3");
   double result = 2538.250420002308894;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_6) {
-  std::string src = "atan(0)^3-155.87633/098.49mod(356-42)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("atan(0)^3-155.87633/098.49mod(356-42)");
   double result = -1.582661488475988;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_7) {
-  std::string src = "3958-sqrt(567^3-567)*ln(1)+289.389013";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("3958-sqrt(567^3-567)*ln(1)+289.389013");
   double result = 4247.389013;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_8) {
-  std::string src = "100010001/3794*0.00034+9999999";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("100010001/3794*0.00034+9999999");
   double result = 10000007.962414428;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_9) {
-  std::string src = "1.0/0.0+cos(180)-1.0/0.0";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("1.0/0.0+cos(180)-1.0/0.0");
   m.CalculateResultFromInput();
   EXPECT_TRUE(std::isnan(m.GetAnswer()));
 }
 
 TEST(calculation_src_test, test_10) {
-  std::string src = "cos(180)-1.0/0.0";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("cos(180)-1.0/0.0");
   m.CalculateResultFromInput();
   EXPECT_TRUE(std::isinf(m.GetAnswer()));
 }
 
 TEST(calculation_src_test, test_11) {
-  std::string src = "tan(50)/3+1.0/0.0";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("tan(50)/3+1.0/0.0");
   m.CalculateResultFromInput();
   EXPECT_TRUE(std::isinf(m.GetAnswer()));
 }
 TEST(calculation_src_test, test_12) {
-  std::string src = "150-cos(180)*150/5+(-180)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("150-cos(180)*150/5+(-180)");
   double result = -12.046197928264263;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_13) {
-  std::string src = "(x+76.3-cos(x*4))/100";
-  s21::MathCalculator m(src, "66");
+  s21::MathCalculator m("(x+76.3-cos(x*4))/100", "66");
   double result = 1.413056357344486;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_14) {
-  std::string src = "cos(76-60)*(35mod7)-542/982";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("cos(76-60)*(35mod7)-542/982");
   double result = -0.55193482688;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_15) {
-  std::string src = "log(1000*ln(200*ln(1000)))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("log(1000*ln(200*ln(1000)))");
   double result = 3.85919608528;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_16) {
-  std::string src = "150-cos(x)*150/5+(-x)";
-  s21::MathCalculator m(src, "180");
+  s21::MathCalculator m("150-cos(x)*150/5+(-x)", "180");
   double result = -12.046197928264263;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_17) {
-  std::string src = "879231*(1/2)+atan(0)/(sqrt(982-237*0.34))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("879231*(1/2)+atan(0)/(sqrt(982-237*0.34))");
   double result = 439615.5;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_18) {
-  std::string src = "879231*(x)+atan(0)/(sqrt(982-x*0.34))";
-  s21::MathCalculator m(src, "1.2");
+  s21::MathCalculator m("879231*(x)+atan(0)/(sqrt(982-x*0.34))", "1.2");
   double result = 1055077.2;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_19) {
-  std::string src = "ln(100000)-78361+asin(2)-(645mod82)^7";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("ln(100000)-78361+asin(2)-(645mod82)^7");
   m.CalculateResultFromInput();
   EXPECT_TRUE(std::isnan(m.GetAnswer()));
 }
 
 TEST(calculation_src_test, test_20) {
-  std::string src = "879231*(x)+atan(0)/(sqrt(982-x*0.34))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("879231*(x)+atan(0)/(sqrt(982-x*0.34))");
   double result = 0;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_21) {
-  std::string src = "8-cos(x^9)*7";
-  s21::MathCalculator m(src, "8");
+  s21::MathCalculator m("8-cos(x^9)*7", "8");
   double result = 3.4785444;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_22) {
-  std::string src = "187/5+9*(ln(10)^3.5)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("187/5+9*(ln(10)^3.5)");
   double result = 204.1237307;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_23) {
-  std::string src = "46/3+5^2*(89+4)-sin(2)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*(89+4)-sin(2)");
   double result = 2339.424035906;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_24) {
-  std::string src = "46/3+5^2*(89+4)*sin(2)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*(89+4)*sin(2)");
   double result = 2129.4498507030432;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_25) {
-  std::string src = "987.65^(1/2)+7*tan(3/0.5)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("987.65^(1/2)+7*tan(3/0.5)");
   double result = 29.3898559;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_26) {
-  std::string src = "2+tan(45)/(7-90*(-4))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("2+tan(45)/(7-90*(-4))");
   double result = 2.0044135;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_27) {
-  std::string src = "96mod2-98*(1/2)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("96mod2-98*(1/2)");
   double result = -49;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_28) {
-  std::string src = "87.9/456+atan(1)-94";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("87.9/456+atan(1)-94");
   double result = -93.0218387;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_29) {
-  std::string src = "sqrt(x)-cos(90+x)/log(10)";
-  s21::MathCalculator m(src, "64");
+  s21::MathCalculator m("sqrt(x)-cos(90+x)/log(10)", "64");
   double result = 8.99808109;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_30) {
-  std::string src = "46/3+5^2*(89+4)*sin(2.5)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*(89+4)*sin(2.5)");
   double result = 1406.78106838;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_31) {
-  std::string src = "";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("");
   double result = 0.0;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_32) {
-  std::string src = "x*x^x";
-  s21::MathCalculator m(src, "7");
+  s21::MathCalculator m("x*x^x", "7");
   double result = 5764801;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
 }
 
 TEST(calculation_src_test, test_33) {
-  std::string src = "x/x^(sqrt(x))";
-  s21::MathCalculator m(src, "4");
+  s21::MathCalculator m("x/x^(sqrt(x))", "4");
   double result = 0.25;
   m.CalculateResultFromInput();
   EXPECT_NEAR(result, m.GetAnswer(), ACCURACY);
--- src/calc_tests/exceptions_tests.cc
+++ src/calc_tests/exceptions_tests.cc
@@ -3,8 +3,7 @@
 #include "../model/s21_model.h"
 
 TEST(validation_src_test_incorrect_first_priority, test_1) {
-  std::string src = "10000000-478.23472-381389+.";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000-478.23472-381389+.");
   try {
     m.CalculateResultFromInput();
     // } catch (...) {
@@ -16,8 +15,7 @@ TEST(validation_src_test_incorrect_first_priority, test_1) {
 }
 
 TEST(validation_src_test_incorrect_first_priority, test_2) {
-  std::string src = "10000000-+-478.23472-381389";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000-+-478.23472-381389");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -25,8 +23,7 @@ TEST(validation_src_test_incorrect_first_priority, test_2) {
 }
 
 TEST(validation_src_test_incorrect_first_priority, test_3) {
-  std::string src = "10000000-sgh-478.23472-381389";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000-sgh-478.23472-381389");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -34,8 +31,7 @@ TEST(validation_src_test_incorrect_first_priority, test_3) {
 }
 
 TEST(validation_src_test_incorrect_first_priority, test_4) {
-  std::string src = "x-0.0001+xx-890317+x";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("x-0.0001+xx-890317+x");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -43,8 +39,7 @@ TEST(validation_src_test_incorrect_first_priority, test_4) {
 }
 
 TEST(validation_src_test_incorrect_first_priority, test_5) {
-  std::string src = "10000000-+478.23472-381389(x)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000-+478.23472-381389(x)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -52,8 +47,7 @@ TEST(validation_src_test_incorrect_first_priority, test_5) {
 }
 
 TEST(validation_src_test_incorrect_first_priority, test_6) {
-  std::string src = "100000.00--478.23472-..381389";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("100000.00--478.23472-..381389");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -61,8 +55,7 @@ TEST(validation_src_test_incorrect_first_priority, test_6) {
 }
 
 TEST(validation_src_test_incorrect_second_priority, test_1) {
-  std::string src = "*569/3901/(199.237*23678.6785)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("*569/3901/(199.237*23678.6785)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -70,8 +63,7 @@ TEST(validation_src_test_incorrect_second_priority, test_1) {
 }
 
 TEST(validation_src_test_incorrect_second_priority, test_2) {
-  std::string src = "10000000-*-478.23472/381389";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000-*-478.23472/381389");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -79,8 +71,7 @@ TEST(validation_src_test_incorrect_second_priority, test_2) {
 }
 
 TEST(validation_src_test_incorrect_second_priority, test_3) {
-  std::string src = ".*367/3782";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m(".*367/3782");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -88,8 +79,7 @@ TEST(validation_src_test_incorrect_second_priority, test_3) {
 }
 
 TEST(validation_src_test_incorrect_second_priority, test_4) {
-  std::string src = "x-0.0001*xx-890317/x";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("x-0.0001*xx-890317/x");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -97,8 +87,7 @@ TEST(validation_src_test_incorrect_second_priority, test_4) {
 }
 
 TEST(validation_src_test_incorrect_second_priority, test_5) {
-  std::string src = "x-0.0001*x-890317//x";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("x-0.0001*x-890317//x");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -106,8 +95,7 @@ TEST(validation_src_test_incorrect_second_priority, test_5) {
 }
 
 TEST(validation_src_test_incorrect_second_priority, test_6) {
-  std::string src = "100000.00/478.23472*..381389";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("100000.00/478.23472*..381389");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -115,8 +103,7 @@ TEST(validation_src_test_incorrect_second_priority, test_6) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_1) {
-  std::string src = "^247^(0.4379)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("^247^(0.4379)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -124,8 +111,7 @@ TEST(validation_src_test_incorrect_third_priority, test_1) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_2) {
-  std::string src = "4269+478=48";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("4269+478=48");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -133,8 +119,7 @@ TEST(validation_src_test_incorrect_third_priority, test_2) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_3) {
-  std::string src = ".367^..3782";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m(".367^..3782");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -142,8 +127,7 @@ TEST(validation_src_test_incorrect_third_priority, test_3) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_4) {
-  std::string src = "x*(0.0001*x)x-890317/x";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("x*(0.0001*x)x-890317/x");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -151,8 +135,7 @@ TEST(validation_src_test_incorrect_third_priority, test_4) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_5) {
-  std::string src = "x-0.0001*x-890317/()-x";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("x-0.0001*x-890317/()-x");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -160,8 +143,7 @@ TEST(validation_src_test_incorrect_third_priority, test_5) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_6) {
-  std::string src = "100000.00/478.23472(381389)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("100000.00/478.23472(381389)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -169,8 +151,7 @@ TEST(validation_src_test_incorrect_third_priority, test_6) {
 }
 
 TEST(validation_src_test_incorrect_third_priority, test_7) {
-  std::string src = "1ee-7+cos(4)*1e+3";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("1ee-7+cos(4)*1e+3");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -178,8 +159,7 @@ TEST(validation_src_test_incorrect_third_priority, test_7) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_1) {
-  std::string src = "sin65)-cos()3";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("sin65)-cos()3");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -187,8 +167,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_1) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_2) {
-  std::string src = "sin(45)+cas(37)-456*log(0)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("sin(45)+cas(37)-456*log(0)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -196,8 +175,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_2) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_3) {
-  std::string src = "32849-log(()";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("32849-log(()");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -205,8 +183,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_3) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_4) {
-  std::string src = "lg(2.75)/log(100)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("lg(2.75)/log(100)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -214,8 +191,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_4) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_5) {
-  std::string src = "ln(2)-atan(0x)*cos(30)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("ln(2)-atan(0x)*cos(30)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -223,8 +199,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_5) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_6) {
-  std::string src = "sqrt(389*389)/tan(5)+log(100x0000)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("sqrt(389*389)/tan(5)+log(100x0000)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -232,8 +207,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_6) {
 }
 
 TEST(validation_src_test_incorrect_fourth_priority, test_7) {
-  std::string src = "tan(439-274)*cos(29) ";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("tan(439-274)*cos(29) ");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -241,8 +215,7 @@ TEST(validation_src_test_incorrect_fourth_priority, test_7) {
 }
 
 TEST(validation_src_test_incorrect, test_1) {
-  std::string src = "46/3+5^2*.(89+4)*sin(2)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*.(89+4)*sin(2)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -250,8 +223,7 @@ TEST(validation_src_test_incorrect, test_1) {
 }
 
 TEST(validation_src_test_incorrect, test_2) {
-  std::string src = "sin(-30)+()-34.5";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("sin(-30)+()-34.5");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -259,8 +231,7 @@ TEST(validation_src_test_incorrect, test_2) {
 }
 
 TEST(validation_src_test_incorrect, test_3) {
-  std::string src = "46/3+5^2*(89+4)*san(2)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*(89+4)*san(2)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -268,8 +239,7 @@ TEST(validation_src_test_incorrect, test_3) {
 }
 
 TEST(validation_src_test_incorrect, test_4) {
-  std::string src = "46/3+5^2*(89+)*sin(2)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*(89+)*sin(2)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -277,8 +247,7 @@ TEST(validation_src_test_incorrect, test_4) {
 }
 
 TEST(validation_src_test_incorrect, test_5) {
-  std::string src = "46/3+5^2*(89+4)*sin2";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("46/3+5^2*(89+4)*sin2");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -286,8 +255,7 @@ TEST(validation_src_test_incorrect, test_5) {
 }
 
 TEST(validation_src_test_incorrect, test_6) {
-  std::string src = "56.-90(cos(3))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("56.-90(cos(3))");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -295,8 +263,7 @@ TEST(validation_src_test_incorrect, test_6) {
 }
 
 TEST(validation_src_test_incorrect, test_7) {
-  std::string src = "56.-90*cos(3))^";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("56.-90*cos(3))^");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -304,8 +271,7 @@ TEST(validation_src_test_incorrect, test_7) {
 }
 
 TEST(validation_src_test_incorrect, test_8) {
-  std::string src = "57+[-9]*(46-24/3)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("57+[-9]*(46-24/3)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -313,8 +279,7 @@ TEST(validation_src_test_incorrect, test_8) {
 }
 
 TEST(validation_src_test_incorrect, test_9) {
-  std::string src = ".*567.98*(-cos(x))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m(".*567.98*(-cos(x))");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -322,8 +287,7 @@ TEST(validation_src_test_incorrect, test_9) {
 }
 
 // TEST(validation_src_test_incorrect, test_10) {
-//   const std::string src = "567.98*(-cos(x))mod";
-//   s21::MathCalculator m(src, "0");
+//   s21::MathCalculator m("567.98*(-cos(x))mod", "0");
 //   try {
 //     m.CalculateResultFromInput();
 //   } catch (...) {
@@ -331,8 +295,7 @@ TEST(validation_src_test_incorrect, test_9) {
 // }
 
 TEST(validation_src_test_incorrect, test_11) {
-  std::string src = "mod5-90/7";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("mod5-90/7");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -340,8 +303,7 @@ TEST(validation_src_test_incorrect, test_11) {
 }
 
 TEST(validation_src_test_incorrect, test_12) {
-  std::string src = "5467.43.290+327-cos(0)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("5467.43.290+327-cos(0)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -349,8 +311,7 @@ TEST(validation_src_test_incorrect, test_12) {
 }
 
 TEST(validation_src_test_incorrect, test_13) {
-  std::string src = "x456-cos(3)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("x456-cos(3)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -358,8 +319,7 @@ TEST(validation_src_test_incorrect, test_13) {
 }
 
 TEST(validation_src_test_incorrect, test_14) {
-  std::string src = "76sin(4)-9823";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("76sin(4)-9823");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -367,8 +327,7 @@ TEST(validation_src_test_incorrect, test_14) {
 }
 
 // TEST(validation_src_test_incorrect, test_15) {
-//   std::string src = "76*sin(4)-9823+";
-//   s21::MathCalculator m(src);
+//   s21::MathCalculator m("76*sin(4)-9823+");
 //   try {
 //     m.CalculateResultFromInput();
 //   } catch (...) {
@@ -376,8 +335,7 @@ TEST(validation_src_test_incorrect, test_14) {
 // }
 
 TEST(validation_src_test_incorrect, test_16) {
-  std::string src = "7.9-gd(x)+652-cos(3)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("7.9-gd(x)+652-cos(3)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -385,8 +343,7 @@ TEST(validation_src_test_incorrect, test_16) {
 }
 
 TEST(validation_src_test_incorrect, test_17) {
-  std::string src = "901/67*1-546+coscos(984)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("901/67*1-546+coscos(984)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -394,8 +351,7 @@ TEST(validation_src_test_incorrect, test_17) {
 }
 
 TEST(validation_src_test_incorrect, test_18) {
-  std::string src = "tan(50)/3+./0.0";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("tan(50)/3+./0.0");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -403,8 +359,7 @@ TEST(validation_src_test_incorrect, test_18) {
 }
 
 TEST(validation_src_test_incorrect, test_19) {
-  std::string src = "(x+76.3-cos(x*4))/100x";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("(x+76.3-cos(x*4))/100x");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -412,8 +367,7 @@ TEST(validation_src_test_incorrect, test_19) {
 }
 
 TEST(validation_src_test_incorrect, test_20) {
-  std::string src = "10000..000+98/100000-cos(90)*(sqrt(625))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000..000+98/100000-cos(90)*(sqrt(625))");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -421,8 +375,7 @@ TEST(validation_src_test_incorrect, test_20) {
 }
 
 TEST(validation_src_test_incorrect, test_21) {
-  std::string src = "10000.000+98/10.00.00-cos(90)*(sqrt(625))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000.000+98/10.00.00-cos(90)*(sqrt(625))");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -430,8 +383,7 @@ TEST(validation_src_test_incorrect, test_21) {
 }
 
 TEST(validation_src_test_incorrect, test_22) {
-  std::string src = "10000000+98/10.0000-cos(90)**(sqrt(625))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000+98/10.0000-cos(90)**(sqrt(625))");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -439,8 +391,7 @@ TEST(validation_src_test_incorrect, test_22) {
 }
 
 TEST(validation_src_test_incorrect, test_23) {
-  std::string src = "10000000+98/10.0000-c-os(90)*(sqrt(625))";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000+98/10.0000-c-os(90)*(sqrt(625))");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -448,8 +399,7 @@ TEST(validation_src_test_incorrect, test_23) {
 }
 
 TEST(validation_src_test_incorrect, test_24) {
-  std::string src = "10000000+98/100000-cos(90)*(sqrt(625),)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("10000000+98/100000-cos(90)*(sqrt(625),)");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -457,8 +407,7 @@ TEST(validation_src_test_incorrect, test_24) {
 }
 
 TEST(validation_src_test_incorrect, test_25) {
-  std::string src = "sqrt(5647*(9-(-78)+))+46^3/250-log(1000)-783*-1.0";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("sqrt(5647*(9-(-78)+))+46^3/250-log(1000)-783*-1.0");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
@@ -466,8 +415,39 @@ TEST(validation_src_test_incorrect, test_25) {
 }
 
 TEST(validation_src_test_incorrect, test_26) {
-  std::string src = "++78-cos(3)";
-  s21::MathCalculator m(src);
+  s21::MathCalculator m("++78-cos(3)");
+  try {
+    m.CalculateResultFromInput();
+  } catch (...) {
+  }
+}
+
+TEST(validation_src_test_incorrect, test_27) {
+  s21::MathCalculator m("+67*(asan(-2)/5)");
+  try {
+    m.CalculateResultFromInput();
+  } catch (...) {
+  }
+}
+
+TEST(validation_src_test_incorrect, test_28) {
+  s21::MathCalculator m("96mod^2-98*(1/2)");
+  try {
+    m.CalculateResultFromInput();
+  } catch (...) {
+  }
+}
+
+TEST(validation_src_test_incorrect, test_29) {
+  s21::MathCalculator m("46/3+5^2*(89+4)*sin.(2)");
+  try {
+    m.CalculateResultFromInput();
+  } catch (...) {
+  }
+}
+
+TEST(validation_src_test_incorrect, test_30) {
+  s21::MathCalculator m(".");
   try {
     m.CalculateResultFromInput();
   } catch (...) {
