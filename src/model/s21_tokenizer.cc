694a31109db3eb094e36818ef611fb561f0b083c
verenach <verenach@student.21-school.ru>
2023-05-04 21:02:36 +0700

    сделала часть проверок через regex

--- src/model/s21_tokenizer.cc
+++ src/model/s21_tokenizer.cc
@@ -1,12 +1,10 @@
 #include "s21_tokenizer.h"
 
 #include "s21_model.h"
+namespace s21 {
+std::vector<Token> Tokenizer::GetTokens() const noexcept { return tokens_; }
 
-std::vector<s21::Token> s21::Tokenizer::GetTokens() const noexcept {
-  return tokens_;
-}
-
-void s21::Tokenizer::CreateTokenOutput() {
+void Tokenizer::CreateTokenOutput() {
   if (input_src_.empty()) return;
 
   CheckHooksInInput();
@@ -18,43 +16,51 @@ void s21::Tokenizer::CreateTokenOutput() {
   FinalInputCheck();
 }
 
-void s21::Tokenizer::SetNewValues(const std::string& input_src,
-                                  const std::string& x) {
+void Tokenizer::SetNewValues(const std::string& input_src,
+                             const std::string& x) {
   // input_src_.clear();
   // token_x_value_.clear();
   input_src_ = input_src;
   token_x_value_ = x;
 }
 
-void s21::Tokenizer::CreateTokens() {
+// ++78-cos(3)
+
+void Tokenizer::CreateTokens() {
   tokens_.clear();
   auto current = input_src_.cbegin();
+
   while (current != input_src_.cend()) {
     if (std::isdigit(*current) || *current == '.') {
       size_t shift = 0;
-      tokens_.emplace_back(std::stod(&(*current), &shift), s21::Priority::kZero,
+      tokens_.emplace_back(std::stod(&(*current), &shift), Priority::kZero,
                            "number");
       current += shift;
-    } else if (std::isalpha(*current)) {
+    }
+
+    else if (std::isalpha(*current)) {
       std::regex base_regex("([a-z]+)");
       std::sregex_iterator regex_it =
           std::sregex_iterator(current, input_src_.cend(), base_regex);
       std::smatch base_match = *regex_it;
+
       FillRecievedToken(base_match.str());
       current += base_match.length();
+
     } else {
       const std::string tmp{*current++};
       FillRecievedToken(tmp);
     }
   }
+
   FindUnarySign();
 }
 
-double s21::Tokenizer::GetXValue() const noexcept {
+double Tokenizer::GetXValue() const noexcept {
   return std::stod(token_x_value_);
 }
 
-void s21::Tokenizer::CheckHooksInInput() const {
+void Tokenizer::CheckHooksInInput() const {
   int count_hooks = 0;
 
   for (auto current_sym = input_src_.cbegin(); current_sym != input_src_.cend();
@@ -69,7 +75,7 @@ void s21::Tokenizer::CheckHooksInInput() const {
   if (count_hooks) throw std::invalid_argument("Hooks error");
 }
 
-void s21::Tokenizer::CheckDotsInInput() const {
+void Tokenizer::CheckDotsInInput() const {
   for (auto current_sym = ++input_src_.cbegin();
        current_sym != input_src_.cend(); ++current_sym) {
     // if (*current_sym == '.' && *(current_sym - 1) == '.')
@@ -81,95 +87,125 @@ void s21::Tokenizer::CheckDotsInInput() const {
   }
 }
 
-void s21::Tokenizer::FillRecievedToken(const std::string& key) {
+void Tokenizer::FillRecievedToken(const std::string& key) {
   auto search_token = valid_tokens.find(key);
+
   if (search_token == valid_tokens.cend())
     throw std::invalid_argument("Invalid token");
+
   tokens_.emplace_back(search_token->second);
+
   if (key == "x") {
     tokens_.back().value_ = GetXValue();
   }
 }
 
-void s21::Tokenizer::FindUnarySign() noexcept {
-  if (tokens_.front().priority_ == s21::Priority::kFirst) {
+void Tokenizer::FindUnarySign() noexcept {
+  if (tokens_.front().priority_ == Priority::kFirst) {
     tokens_.front() = FillUnarySign(tokens_.front());
   }
+
   for (auto current = tokens_.begin(); current != tokens_.end(); ++current) {
-    if ((current->priority_ == s21::Priority::kFirst ||
-         current->priority_ == s21::Priority::kSecond ||
-         current->priority_ == s21::Priority::kFourth ||
-         current->type_ == "(" || current->type_ == "^" ||
-         current->type_ == "%") &&
-        (current + 1)->priority_ == s21::Priority::kFirst) {
+    if ((current->priority_ == Priority::kFirst ||
+         current->priority_ == Priority::kSecond ||
+         current->priority_ == Priority::kFourth || current->type_ == "(" ||  //
+         current->type_ == "^" ||                                             //
+         current->type_ == "%")                                               //
+        && (current + 1)->priority_ == Priority::kFirst)
+
+    {
       *(current + 1) = FillUnarySign(*(current + 1));
     }
   }
 }
 
-s21::Token s21::Tokenizer::FillUnarySign(Token& other) noexcept {
-  other.priority_ = s21::Priority::kThird;
+Token Tokenizer::FillUnarySign(Token& other) noexcept {
+  other.priority_ = Priority::kThird;
   other.is_binary_ = false;
+
   if (other.type_ == "sum") other.type_ = "u_plus";
   if (other.type_ == "sub") other.type_ = "u_minus";
   return other;
 }
 
-void s21::Tokenizer::CheckHooksAfterFunctions() const {
+void Tokenizer::CheckHooksAfterFunctions() const {
   for (auto current = tokens_.cbegin(); current != tokens_.cend(); ++current) {
-    if (current->priority_ == s21::Priority::kFourth &&
-        (current + 1)->type_ != "(")
+    if (current->priority_ == Priority::kFourth && (current + 1)->type_ != "(")
       throw std::invalid_argument("Function without hooks");
   }
 }
 
-void s21::Tokenizer::CheckXValue() const {
+void Tokenizer::CheckXValue() const {
+  if (!std::regex_match(token_x_value_,
+                        std::regex("[0-9]*.[0-9]*")))  // 0.25 .25 0. .
+    throw std::invalid_argument("Incorrect x value HERE");
+
   if (token_x_value_ == ".") throw std::invalid_argument("Incorrect x value");
 
-  for (auto iterator_to_num = token_x_value_.cbegin();
-       iterator_to_num != token_x_value_.cend(); ++iterator_to_num) {
-    if ((!std::isdigit(*iterator_to_num) && *iterator_to_num != '.') ||
-        (*iterator_to_num == '.' && *(iterator_to_num + 1) == '.')) {
-      throw std::invalid_argument("Incorrect x value");
-    }
-  }
+  // if (std::regex_search(token_x_value_, std::regex(".[0-9]*.")))
+  //   throw std::invalid_argument("Incorrect x value");
+
+  // for (auto iterator_to_num = token_x_value_.cbegin();
+  //      iterator_to_num != token_x_value_.cend(); ++iterator_to_num) {
+  //   if ((!std::isdigit(*iterator_to_num) && *iterator_to_num != '.') ||
+  //       (*iterator_to_num == '.' && *(iterator_to_num + 1) == '.') ||
+  //       (*iterator_to_num == '.' && token_x_value_.length() == 1)) {
+  //     throw std::invalid_argument("Incorrect x value");
+  //   }
+  // }
 }
 
-void s21::Tokenizer::CheckEdgeValues() const {
+void Tokenizer::CheckEdgeValues() const {
   if (tokens_.front().is_binary_)
     throw std::invalid_argument("Missing operand");
   if (tokens_.back().is_binary_ ||
-      tokens_.back().priority_ == s21::Priority::kFourth)
+      tokens_.back().priority_ == Priority::kFourth)
     throw std::invalid_argument("Missing operand");
 }
 
-void s21::Tokenizer::FinalInputCheck() const {
+void Tokenizer::FinalInputCheck() const {
   for (auto current_token = tokens_.cbegin(); current_token != --tokens_.cend();
        ++current_token) {
-    if (current_token->priority_ == s21::Priority::kZero &&
-        ((current_token + 1)->priority_ == s21::Priority::kFourth ||
+    // CheckTokens(current_token->type_, (current_token + 1)->type_);
+
+    if (current_token->priority_ == Priority::kZero &&
+        ((current_token + 1)->priority_ == Priority::kFourth ||
          (current_token + 1)->type_ == "(")) {
       throw std::invalid_argument("Missing operator");
     }
-    if ((current_token->priority_ == s21::Priority::kSecond ||
-         current_token->priority_ == s21::Priority::kFirst) &&
+    if ((current_token->priority_ == Priority::kSecond ||
+         current_token->priority_ == Priority::kFirst) &&
         ((current_token + 1)->type_ == ")" ||
          (current_token + 1)->type_ == "^" ||
          (current_token + 1)->type_ == "%")) {
       throw std::invalid_argument("Missing value");
     }
     if (current_token->is_binary_ &&
-        ((current_token + 1)->priority_ != s21::Priority::kZero &&
-         (current_token + 1)->priority_ != s21::Priority::kFourth &&
+        ((current_token + 1)->priority_ != Priority::kZero &&
+         (current_token + 1)->priority_ != Priority::kFourth &&
          (current_token + 1)->type_ != "("))
       throw std::invalid_argument("Missing value");
+
     // if (((current_token->priority_ == (current_token + 1)->priority_ ||
     //       current_token->type_ == (current_token + 1)->type_)) &&
     //     ((current_token->type_ != "(" || current_token->type_ != ")") &&
-    //      (current_token + 1)->priority_ != s21::Priority::kThird)) {
+    //      (current_token + 1)->priority_ != Priority::kThird)) {
     //   std::cout << current_token->type_ << " " << (current_token + 1)->type_
     //             << std::endl;
     //   throw std::invalid_argument("Two equal tokens in a row");
     // }
   }
 }
+
+// bool CheckTokens(std::string type1, std::string type2) {
+//   int i=0,j=0;
+
+//   if (type1 == "x")i = 0;
+
+// std::vector<std::vector<bool>> matrix = {
+
+// };
+
+// matrix[i][j];
+// }
+}  // namespace s21
