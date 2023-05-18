46b3dde647e72e8f7dccc0faa9204fedd3a54c58
verenach <verenach@student.21-school.ru>
2023-05-18 14:48:41 +0700

    добавила классы model, controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 
+#include "model/s21_model.h"
+#include "model/s21_credit_calculator.h"
+#include "controller/s21_controller.h"
+
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.setFixedSize(650, 575);
-    w.show();
+    MainWindow view;
+    view.setFixedSize(650, 575);
+    view.show();
+    s21::MathCalculator model;
+    s21::CreditCalculator credit;
+    s21::Controller controller;
     return a.exec();
 }
