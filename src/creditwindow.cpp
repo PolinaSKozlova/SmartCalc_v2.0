323adae19f4c98d0f46cf86093bbc76209834f88
verenach <verenach@student.21-school.ru>
2023-05-19 17:46:03 +0700

    добавила сигналы для кнопок

--- src/creditwindow.cpp
+++ src/creditwindow.cpp
@@ -1,5 +1,6 @@
 #include "creditwindow.h"
 #include "ui_creditwindow.h"
+#include <QErrorMessage>
 
 
 CreditWindow::CreditWindow(QWidget *parent) :
@@ -7,6 +8,9 @@ CreditWindow::CreditWindow(QWidget *parent) :
     ui(new Ui::CreditWindow)
 {
     ui->setupUi(this);
+    setFixedSize(891,721);
+    ui->months->setChecked(true);
+    ui->a_type->setChecked(true);
 }
 
 CreditWindow::~CreditWindow()
@@ -14,4 +18,42 @@ CreditWindow::~CreditWindow()
     delete ui;
 }
 
+
+
+void CreditWindow::on_months_clicked()
+{
+    credit_data_.term_in_years_ = false;
+}
+
+
+void CreditWindow::on_years_clicked()
+{
+    credit_data_.term_in_years_ = true;
+}
+
+
+void CreditWindow::on_pushButton_clicked()
+{
+   try {
+        credit_data_.CheckCreditValues(ui->credit_sum->text().toStdString(),ui->credit_term->text().toStdString(), ui->credit_range->text().toStdString());
+    } catch(std::invalid_argument &e) {
+        QString msg_error = e.what();
+//        QMessageBox::critical(this, "ERROR", msg_error);
+    }
+
+}
+
+
+void CreditWindow::on_pushButton_2_clicked()
+{
+    ui->credit_range->clear();
+    ui->credit_sum->clear();
+    ui->credit_term->clear();
+    ui->result_monthly_pay->clear();
+    ui->result_percents->clear();
+    ui->result_total->clear();
+    ui->list_of_payments->clear();
+    ui->months->setChecked(true);
+    ui->a_type->setChecked(true);
+}
 
--- src/creditwindow.h
+++ src/creditwindow.h
@@ -2,6 +2,7 @@
 #define CREDITWINDOW_H
 
 #include <QMainWindow>
+#include "model/s21_credit_calculator.h"
 
 namespace Ui {
 class CreditWindow;
@@ -15,8 +16,18 @@ public:
     explicit CreditWindow(QWidget *parent = nullptr);
     ~CreditWindow();
 
+private slots:
+    void on_months_clicked();
+
+    void on_years_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::CreditWindow *ui;
+    s21::CreditInformation credit_data_;
 };
 
 #endif // CREDITWINDOW_H
