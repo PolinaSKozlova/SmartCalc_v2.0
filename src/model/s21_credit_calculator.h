36ad1cc64e7ff9cd4b4ae0cc3f7dea7d086b523f
verenach <verenach@student.21-school.ru>
2023-05-19 18:35:42 +0700

    добавила конструктор и оператор копирования

--- src/model/s21_credit_calculator.h
+++ src/model/s21_credit_calculator.h
@@ -11,14 +11,26 @@ class CreditInformation {
   CreditInformation(double credit_sum, int credit_term, double interest_rate,
                     const std::vector<double>& monthly_payment,
                     double payble_percents, double total_sum,
-                    bool term_in_years)
+                    bool credit_type = false, bool term_in_years = false)
       : credit_sum_(credit_sum),
         credit_term_(credit_term),
         interest_rate_(interest_rate),
         monthly_payment_(monthly_payment),
         payble_percents_(payble_percents),
         total_sum_(total_sum),
+        credit_type_(credit_type),
         term_in_years_(term_in_years) {}
+  CreditInformation(const CreditInformation& other) { *this = other; }
+  CreditInformation& operator=(const CreditInformation& other) {
+    credit_sum_ = other.credit_sum_;
+    credit_term_ = other.credit_term_;
+    interest_rate_ = other.interest_rate_;
+    monthly_payment_ = other.monthly_payment_;
+    payble_percents_ = other.payble_percents_;
+    total_sum_ = other.total_sum_;
+    credit_type_ = other.credit_type_;
+    term_in_years_ = other.term_in_years_;
+  }
   ~CreditInformation() = default;
   CreditInformation GetInformation() const noexcept { return *this; }
   void CheckCreditValues(const std::string& sum, const std::string& term,
@@ -31,6 +43,7 @@ class CreditInformation {
   std::vector<double> monthly_payment_;
   double payble_percents_;
   double total_sum_;
+  bool credit_type_;
   bool term_in_years_;
 };
 
