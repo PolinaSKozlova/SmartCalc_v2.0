5cad08a049b3c758ec07ba4d69e10dd61bac2ab0
verenach <verenach@student.21-school.ru>
2023-05-22 18:10:00 +0700

    добавила namespace, перенесла реализацию метода FromYearsToMonths в хедер, добавила const к геттерам

--- src/model/s21_credit_calculator.cc
+++ src/model/s21_credit_calculator.cc
@@ -2,22 +2,20 @@
 
 #include <regex>
 
+namespace s21 {
 static double RoundNumber(double value) { return round(value * 100) / 100; }
 
-void s21::CreditCalculator::AnnuitetMethod() noexcept {
-  data_.FromYearsToMonths();
-  data_.interest_rate_ = data_.interest_rate_ / 100 / 12;
-  data_.monthly_payment_.push_back(RoundNumber(
-      data_.credit_sum_ *
-      (data_.interest_rate_ /
-       (1 - pow((1 + data_.interest_rate_), (data_.credit_term_ * -1))))));
-  data_.total_sum_ = data_.monthly_payment_.front() * data_.credit_term_;
-  data_.payble_percents_ = data_.total_sum_ - data_.credit_sum_;
+void CreditCalculator::SetTermInYears(bool value) noexcept {
+  data_.term_in_years_ = value;
 }
 
-void s21::CreditCalculator::CheckCreditValues(const std::string& sum,
-                                              const std::string& term,
-                                              const std::string& range) {
+void CreditCalculator::SetCreditType(bool value) noexcept {
+  data_.is_differntiated = value;
+}
+
+void CreditCalculator::CheckCreditValues(const std::string& sum,
+                                         const std::string& term,
+                                         const std::string& range) {
   if (!std::regex_match(sum, std::regex("(\\d*[.]?\\d*)")))
     throw std::invalid_argument("Incorrect sum value");
   if (!std::regex_match(term, std::regex("\\d*")))
@@ -29,7 +27,18 @@ void s21::CreditCalculator::CheckCreditValues(const std::string& sum,
   data_.interest_rate_ = std::stod(range);
 }
 
-void s21::CreditCalculator::DifferntiatedMethod() noexcept {
+void CreditCalculator::AnnuitetMethod() noexcept {
+  data_.FromYearsToMonths();
+  data_.interest_rate_ = data_.interest_rate_ / 100 / 12;
+  data_.monthly_payment_.push_back(RoundNumber(
+      data_.credit_sum_ *
+      (data_.interest_rate_ /
+       (1 - pow((1 + data_.interest_rate_), (data_.credit_term_ * -1))))));
+  data_.total_sum_ = data_.monthly_payment_.front() * data_.credit_term_;
+  data_.payble_percents_ = data_.total_sum_ - data_.credit_sum_;
+}
+
+void CreditCalculator::DifferntiatedMethod() noexcept {
   data_.FromYearsToMonths();
   data_.interest_rate_ = data_.interest_rate_ / 100;
   double pay = 0;
@@ -46,7 +55,4 @@ void s21::CreditCalculator::DifferntiatedMethod() noexcept {
   data_.total_sum_ += perc_sum;
   data_.payble_percents_ = perc_sum;
 }
-
-int s21::CreditInformation::FromYearsToMonths() {
-  return term_in_years_ ? credit_term_ * 12 : credit_term_;
-}
+};  // namespace s21
