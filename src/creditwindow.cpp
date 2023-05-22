70ae5005ba5ff28fa6325ebec0b140b16fcbc2f4
verenach <verenach@student.21-school.ru>
2023-05-22 18:55:37 +0700

    добавила вывод списка платежей для дифференциированного способа расчета

--- src/creditwindow.cpp
+++ src/creditwindow.cpp
@@ -42,6 +42,14 @@ void CreditWindow::on_count_clicked()
         controller_credit_->CountCredit(ui->credit_sum->text().toStdString(),ui->credit_term->text().toStdString(), ui->credit_range->text().toStdString());
         ui->result_total->setText(QString::number(controller_credit_->GetCreditData().total_sum_));
         ui->result_percents->setText(QString::number(controller_credit_->GetCreditData().payble_percents_));
+        if(controller_credit_->GetCreditData().is_differntiated){
+            ui->result_monthly_pay->setText(QString::number(controller_credit_->GetCreditData().monthly_payment_.front(),'q',2) + "..." + QString::number(controller_credit_->GetCreditData().monthly_payment_.back(),'q',2));
+            for(int i = 0; i < controller_credit_->GetCreditData().monthly_payment_.size(); ++i) {
+                ui->list_of_payments->insertPlainText(QString::number(i+1)+") " + QString::number(controller_credit_->GetCreditData().monthly_payment_[i]) + "\n");
+            }
+        } else {
+            ui->result_monthly_pay->setText(QString::number(controller_credit_->GetCreditData().monthly_payment_.front(),'q',2));
+        }
     } catch(std::invalid_argument &e) {
         QMessageBox::critical(this, "ERROR", e.what());
     }
