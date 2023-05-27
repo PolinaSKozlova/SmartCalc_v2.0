a4aa2f026e0c653316c80ee48ddce5bcbc1e5342
verenach <verenach@student.21-school.ru>
2023-05-27 16:54:40 +0700

    добавила проверку срок

--- src/model/s21_credit_calculator.cc
+++ src/model/s21_credit_calculator.cc
@@ -72,6 +72,8 @@ void CreditCalculator::CheckTerms() const {
   if ((data_.credit_term_ > 100 && data_.term_in_years_) ||
       (data_.credit_term_ > 1200 && !data_.term_in_years_))
     throw std::invalid_argument("Too long term");
+  if (!data_.credit_term_)
+    throw std::invalid_argument("Credit term can't be 0");
 }
 
 void CreditCalculator::CheckSum() const {
