d22b3c7692a131f166f924a87b396f9fc286c1a9
verenach <verenach@student.21-school.ru>
2023-05-31 15:42:19 +0700

    добавила расширение виджета в грайик

--- src/plotwindow.cpp
+++ src/plotwindow.cpp
@@ -14,19 +14,17 @@ PlotWindow::~PlotWindow() { delete ui; }
 void PlotWindow::MakePlotArea(
     s21::MaxMinValues mmv,
     std::pair<std::vector<double>, std::vector<double>> vector_of_pairs) {
-  QVector<double> x_axis(vector_of_pairs.first.begin(),
-                         vector_of_pairs.first.end());
-  QVector<double> y_axis(vector_of_pairs.second.begin(),
-                         vector_of_pairs.second.end());
+  QVector<double> x_vector(vector_of_pairs.first.begin(),
+                           vector_of_pairs.first.end());
+  QVector<double> y_vector(vector_of_pairs.second.begin(),
+                           vector_of_pairs.second.end());
   ui->graph_widget->addGraph();
   //  ui->graph_widget->legend->setVisible(true);
-  ui->graph_widget->xAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
-  ui->graph_widget->yAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
   ui->graph_widget->setInteraction(QCP::iRangeZoom, true);
   ui->graph_widget->setInteraction(QCP::iRangeDrag, true);
   ui->graph_widget->xAxis->setSubTickPen(QPen(Qt::blue, 1));
   ui->graph_widget->yAxis->setSubTickPen(QPen(Qt::blue, 1));
-  ui->graph_widget->graph(0)->setData(x_axis, y_axis);
+  ui->graph_widget->graph(0)->setData(x_vector, y_vector);
   ui->graph_widget->graph(0)->setPen(QPen(Qt ::green, 2));
   ui->graph_widget->graph(0)->setLineStyle(QCPGraph::lsLine);
   SetValueForAxisX(mmv.min_x_, mmv.max_x_, "X");
@@ -44,9 +42,11 @@ void PlotWindow::ClearPlot() {
 void PlotWindow::SetValueForAxisX(double min, double max, QString name) {
   ui->graph_widget->xAxis->setRange(min, max);
   ui->graph_widget->xAxis->setLabel(name);
+  ui->graph_widget->xAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
 }
 
 void PlotWindow::SetValueForAxisY(double min, double max, QString name) {
   ui->graph_widget->yAxis->setRange(min, max);
   ui->graph_widget->yAxis->setLabel(name);
+  ui->graph_widget->yAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
 }
